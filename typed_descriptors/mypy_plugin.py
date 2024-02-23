dcfb6059c6c27db520441e2f2d50e5f25038043e
sg495 <sg495@users.noreply.github.com>
2024-02-23 18:47:01 +0000

    Minor refactoring of mypy_plugin

--- typed_descriptors/mypy_plugin.py
+++ typed_descriptors/mypy_plugin.py
@@ -6,32 +6,30 @@ the :mod`typed_descriptors` library.
 from __future__ import annotations
 from collections.abc import Callable
 import typing
-from mypy.types import CallableType, Instance, get_proper_type, Type
 from mypy.plugin import FunctionContext, Plugin
+from mypy.types import CallableType, Instance, get_proper_type, Type
 
-
-def typed_descriptor_ret_type_arg0(ctx: FunctionContext) -> Type:
+def typed_descriptor_hook(ctx: FunctionContext) -> Type:
     """
     Extracts the descriptor type and assigns it to the generic type parameter,
     presuming that the generic type parameter appears at index 0.
     """
     assert ctx.arg_types and ctx.arg_types[0]
-    descriptor_t_constr = get_proper_type(ctx.arg_types[0][0])
+    _descriptor_t = get_proper_type(ctx.arg_types[0][0])
     ret_t = get_proper_type(ctx.default_return_type)
-    if isinstance(descriptor_t_constr, CallableType):
-        descriptor_t = descriptor_t_constr.ret_type
+    if isinstance(_descriptor_t, CallableType):
+        descriptor_t = _descriptor_t.ret_type
         if isinstance(ret_t, Instance):
             args = list(ret_t.args)
             args[0] = descriptor_t
-            ret_t = ret_t.copy_modified(args=tuple(args))
+            return ret_t.copy_modified(args=tuple(args))
+        return ret_t
     return ret_t
 
-
 _function_hooks = {
-    "typed_descriptors.attr.Attr": typed_descriptor_ret_type_arg0,
-    "typed_descriptors.prop.Prop": typed_descriptor_ret_type_arg0,
+    "typed_descriptors.attr.Attr": typed_descriptor_hook,
+    "typed_descriptors.prop.Prop": typed_descriptor_hook,
 }
-
 
 class TypedDescriptorsPlugin(Plugin):
     """
@@ -46,7 +44,6 @@ class TypedDescriptorsPlugin(Plugin):
         if hook is not None:
             return hook
         return super().get_function_hook(fullname)
-
 
 def plugin(version: str) -> typing.Type[TypedDescriptorsPlugin]:
     """
